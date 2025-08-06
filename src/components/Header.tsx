b324dee2e74e6a078062e28ceac6e213bd94a1e9
이준학 <0710june@naver.com>
2025-08-06 22:13:04 +0900

    기본 세팅 완료

---
+++ src/components/Header.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import { Fade, Flex, Line, ToggleButton } from "@once-ui-system/core";
+
+import { routes, display, person, about, blog, work } from "@/resources";
+import { ThemeToggle } from "./ThemeToggle";
+import styles from "./Header.module.scss";
+
+type TimeDisplayProps = {
+  timeZone: string;
+  locale?: string; // Optionally allow locale, defaulting to 'en-GB'
+};
+
+const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = "en-GB" }) => {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        timeZone,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      };
+      const timeString = new Intl.DateTimeFormat(locale, options).format(now);
+      setCurrentTime(timeString);
+    };
+
+    updateTime();
+    const intervalId = setInterval(updateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timeZone, locale]);
+
+  return <>{currentTime}</>;
+};
+
+export default TimeDisplay;
+
+export const Header = () => {
+  const pathname = usePathname() ?? "";
+
+  return (
+    <>
+      <Fade fillWidth position="fixed" height="80" zIndex={9} />
+      <Flex
+        fitHeight
+        position="unset"
+        className={styles.position}
+        as="header"
+        zIndex={9}
+        fillWidth
+        padding="8"
+        horizontal="center"
+        data-border="rounded"
+      >
+        <Flex paddingLeft="12" fillWidth vertical="center" textVariant="body-default-s">
+          {display.location && <Flex s={{ hide: true }}>{person.location}</Flex>}
+        </Flex>
+        <Flex fillWidth horizontal="center">
+          <Flex
+            background="page"
+            border="neutral-alpha-weak"
+            radius="m-4"
+            shadow="l"
+            padding="4"
+            horizontal="center"
+            zIndex={1}
+          >
+            <Flex gap="4" vertical="center" textVariant="body-default-s" suppressHydrationWarning>
+              {routes["/"] && (
+                <ToggleButton prefixIcon="home" href="/" selected={pathname === "/"} />
+              )}
+              <Line background="neutral-alpha-medium" vert maxHeight="24" />
+              {routes["/about"] && (
+                <>
+                  <Flex s={{ hide: true }}>
+                    <ToggleButton
+                      prefixIcon="person"
+                      href="/about"
+                      label={about.label}
+                      selected={pathname === "/about"}
+                    />
+                  </Flex>
+                  <Flex hide s={{ hide: false }}>
+                    <ToggleButton
+                      prefixIcon="person"
+                      href="/about"
+                      selected={pathname === "/about"}
+                    />
+                  </Flex>
+                </>
+              )}
+              {routes["/work"] && (
+                <>
+                  <Flex s={{ hide: true }}>
+                    <ToggleButton
+                      prefixIcon="grid"
+                      href="/work"
+                      label={work.label}
+                      selected={pathname.startsWith("/work")}
+                    />
+                  </Flex>
+                  <Flex hide s={{ hide: false }}>
+                    <ToggleButton
+                      prefixIcon="grid"
+                      href="/work"
+                      selected={pathname.startsWith("/work")}
+                    />
+                  </Flex>
+                </>
+              )}
+              {routes["/blog"] && (
+                <>
+                  <Flex s={{ hide: true }}>
+                    <ToggleButton
+                      prefixIcon="book"
+                      href="/blog"
+                      label={blog.label}
+                      selected={pathname.startsWith("/blog")}
+                    />
+                  </Flex>
+                  <Flex hide s={{ hide: false }}>
+                    <ToggleButton
+                      prefixIcon="book"
+                      href="/blog"
+                      selected={pathname.startsWith("/blog")}
+                    />
+                  </Flex>
+                </>
+              )}
+              {display.themeSwitcher && (
+                <>
+                  <Line background="neutral-alpha-medium" vert maxHeight="24" />
+                  <ThemeToggle />
+                </>
+              )}
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex fillWidth horizontal="end" vertical="center">
+          <Flex
+            paddingRight="12"
+            horizontal="end"
+            vertical="center"
+            textVariant="body-default-s"
+            gap="20"
+          >
+            <Flex s={{ hide: true }}>{display.time && <TimeDisplay timeZone={person.location} />}</Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
